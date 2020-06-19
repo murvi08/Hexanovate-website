b871dd2c78933452acf4877bd1f697a73b1e98b4
urviHexanovate <urvimandge@gmail.com>
2020-06-19 16:40:29 +0530

    django settings commented

--- hexanovate_website/settings.py
+++ hexanovate_website/settings.py
@@ -1,6 +1,6 @@
 import os
 
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from hexanovate_website.config import SMTP_HOST, SMTP_HOST_EMAIL, SMTP_HOST_PASSWORD
@@ -258,4 +258,4 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
